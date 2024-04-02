70f327f6bb91d36ec770a4af967f314e93358d03
Myroslava <mir_ok@ukr.net>
2024-04-02 17:13:41 +0300

    selectors

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { selectNameFilter } from "../filters/slice";
+import { selectNameFilter } from "../filters/selectors";
 
 export const selectContacts = (state) => state.contacts.items;
 
